67677e1b366d954aba4c12af1c3ba22addb5f857
adrianbunea <adrian_bunea28@yahoo.com>
2019-05-12 16:07:18 +0300

    Make board creation function
    Function generates all the squares and groups them in a panel. Will probably move into form load event

--- GrandChess/GrandChess/Form1.Designer.cs
+++ GrandChess/GrandChess/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace GrandChess
 {
-    partial class Form1
+    partial class mainWindow
     {
         /// <summary>
         /// Required designer variable.
@@ -28,13 +28,60 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.chessBoardPanel = new System.Windows.Forms.Panel();
+            this.menuStrip = new System.Windows.Forms.MenuStrip();
+            this.gameToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // chessBoardPanel
+            // 
+            this.chessBoardPanel.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.chessBoardPanel.Location = new System.Drawing.Point(30, 100);
+            this.chessBoardPanel.Margin = new System.Windows.Forms.Padding(20);
+            this.chessBoardPanel.Name = "chessBoardPanel";
+            this.chessBoardPanel.Size = new System.Drawing.Size(720, 720);
+            this.chessBoardPanel.TabIndex = 0;
+            // 
+            // menuStrip
+            // 
+            this.menuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.gameToolStripMenuItem});
+            this.menuStrip.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip.Name = "menuStrip";
+            this.menuStrip.Size = new System.Drawing.Size(1264, 24);
+            this.menuStrip.TabIndex = 1;
+            this.menuStrip.Text = "menuStrip1";
+            // 
+            // gameToolStripMenuItem
+            // 
+            this.gameToolStripMenuItem.Name = "gameToolStripMenuItem";
+            this.gameToolStripMenuItem.Size = new System.Drawing.Size(77, 20);
+            this.gameToolStripMenuItem.Text = "New Game";
+            this.gameToolStripMenuItem.Click += new System.EventHandler(this.NewGame);
+            // 
+            // mainWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(1264, 921);
+            this.Controls.Add(this.chessBoardPanel);
+            this.Controls.Add(this.menuStrip);
+            this.MainMenuStrip = this.menuStrip;
+            this.Name = "mainWindow";
+            this.Text = "Grand Chess";
+            this.menuStrip.ResumeLayout(false);
+            this.menuStrip.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel chessBoardPanel;
+        private System.Windows.Forms.MenuStrip menuStrip;
+        private System.Windows.Forms.ToolStripMenuItem gameToolStripMenuItem;
     }
 }
 

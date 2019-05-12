403ccda062a659c098fdc74cb76e72e51924fc50
adrianbunea <adrian_bunea28@yahoo.com>
2019-05-12 17:00:48 +0300

    Move board creation to 'mainWindow' constructor

--- GrandChess/GrandChess/Form1.cs
+++ GrandChess/GrandChess/Form1.cs
@@ -15,28 +15,32 @@ namespace GrandChess
         public mainWindow()
         {
             InitializeComponent();
+            CreateBoard();
         }
 
-        private void NewGame(object sender, EventArgs e)
+        private void CreateBoard()
         {
             int squareSize = chessBoardPanel.Size.Height / 10;
             for (int i = 0; i < 100; i++)
             {
-                int row = i/10;
-                int column = i%10;
+                int row = i / 10;
+                int column = i % 10;
 
                 PictureBox square = new PictureBox()
                 {
                     //Image = Image.FromFile(@"Assets/ChessPieces/BlackBishop.png"),
                     SizeMode = PictureBoxSizeMode.Zoom,
                     Size = new Size(squareSize, squareSize),
-                    BackColor = ((row+column) % 2 == 0 ? Color.White : Color.DimGray),
-                    Location = new Point(column*squareSize, row*squareSize)
+                    BackColor = ((row + column) % 2 == 0 ? Color.White : Color.DimGray),
+                    Location = new Point(column * squareSize, row * squareSize)
                 };
 
                 chessBoardPanel.Controls.Add(square);
             }
-            
+        }
+
+        private void NewGame(object sender, EventArgs e)
+        {
         }
     }
 }

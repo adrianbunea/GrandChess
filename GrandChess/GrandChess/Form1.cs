52c854e433ac785a73c817840ba00e45d2aa222b
adrianbunea <adrian_bunea28@yahoo.com>
2019-05-12 20:46:39 +0300

    Create chess board initialization function
    The function initializes the board from a txt file that specifies the positions of the pieces at the beginning

--- GrandChess/GrandChess/Form1.cs
+++ GrandChess/GrandChess/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -28,7 +29,6 @@ namespace GrandChess
 
                 PictureBox square = new PictureBox()
                 {
-                    //Image = Image.FromFile(@"Assets/ChessPieces/BlackBishop.png"),
                     SizeMode = PictureBoxSizeMode.Zoom,
                     Size = new Size(squareSize, squareSize),
                     BackColor = ((row + column) % 2 == 0 ? Color.White : Color.DimGray),
@@ -41,6 +41,46 @@ namespace GrandChess
 
         private void NewGame(object sender, EventArgs e)
         {
+            Dictionary<string, string> piecePaths = new Dictionary<string, string>
+            {
+                {"br" , @"Assets/ChessPieces/BlackRook.png"},
+                {"bkn", @"Assets/ChessPieces/BlackKnight.png"},
+                {"bb" , @"Assets/ChessPieces/BlackBishop.png"},
+                {"bq" , @"Assets/ChessPieces/BlackQueen.png"},
+                {"bk" , @"Assets/ChessPieces/BlackKing.png"},
+                {"bm" , @"Assets/ChessPieces/BlackMarshall.png"},
+                {"bc" , @"Assets/ChessPieces/BlackCardinal.png"},
+                {"bp" , @"Assets/ChessPieces/BlackPawn.png"},
+                {"wr" , @"Assets/ChessPieces/WhiteRook.png"},
+                {"wkn", @"Assets/ChessPieces/WhiteKnight.png"},
+                {"wb" , @"Assets/ChessPieces/WhiteBishop.png"},
+                {"wq" , @"Assets/ChessPieces/WhiteQueen.png"},
+                {"wk" , @"Assets/ChessPieces/WhiteKing.png"},
+                {"wm" , @"Assets/ChessPieces/WhiteMarshall.png"},
+                {"wc" , @"Assets/ChessPieces/WhiteCardinal.png"},
+                {"wp" , @"Assets/ChessPieces/WhitePawn.png"}
+            };
+
+            string[] rows = File.ReadAllLines(@"../../Assets/InitialSetup.txt");
+            List<string> pieces = new List<string>();
+            for (int i = 0; i < 10; i++)
+            {
+                pieces.AddRange(rows[i].Split(','));
+            }
+
+            for (int i = 0; i < pieces.Count; i++)
+            {
+                pieces[i] = pieces[i].Trim();
+            }
+
+            foreach (PictureBox square in chessBoardPanel.Controls)
+            {
+                string key = pieces[chessBoardPanel.Controls.IndexOf(square)];
+                if (key != "0")
+                {
+                    square.Image = Image.FromFile(piecePaths[key]);
+                }
+            }
         }
     }
 }
